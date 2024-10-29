b724009ea7a60b2aa53ae4e580e1bf44e37c73e7
Martin Arrostito <arrrostitomartin@gmail.com>
2024-10-29 19:53:50 -0300

    sumo primperos estilos a la app

--- proyect-react-js/src/app.jsx
+++ proyect-react-js/src/app.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import './App.css';
+
 
 function App() {
     return (
